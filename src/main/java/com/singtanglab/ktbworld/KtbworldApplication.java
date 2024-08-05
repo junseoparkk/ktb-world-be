0f620ec4b30ae7d3f4f0138b66c1ef97689fd980
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-08-05 23:10:59 +0900

    project settings

---
+++ src/main/java/com/singtanglab/ktbworld/KtbworldApplication.java
@@ -0,0 +1,13 @@
+package com.singtanglab.ktbworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtbworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KtbworldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/singtanglab/ktbworld/KtbworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.singtanglab.ktbworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KtbworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
